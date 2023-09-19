1e68c23c4aeeafe715a97cb3d8e6a4e5c511c402
gorhel5 <Yaroslavlit@gmail.com>
2023-09-19 22:11:35 +0300

    init

---
+++ constants/catalog.js
@@ -0,0 +1,63 @@
+const CATALOG = [
+    {
+        id: 'el1',
+        name: 'Gan 356 XS',
+        img: 'https://i.ibb.co/bXscBYW/g356.jpg',
+        price: 2000,
+    },
+    {
+        id: 'el2',
+        name: 'Valk 3 Power',
+        img: 'https://i.ibb.co/frwSjhS/02.jpg',
+        price: 700,
+    },
+    {
+        id: 'el3',
+        name: 'MGC Elite',
+        img: 'https://i.ibb.co/HYbfFC9/03.jpg',
+        price: 600,
+    },
+    {
+        id: 'el4',
+        name: 'Gan 11 M Pro',
+        img: 'https://i.ibb.co/bNFY1pK/04.jpg',
+        price: 2500,
+    },
+    {
+        id: 'el5',
+        name: 'QiYi MoFangGe Thunderclap v2',
+        img: 'https://i.ibb.co/nfnsqxF/05.jpg',
+        price: 250,
+    },
+    {
+        id: 'el6',
+        name: 'MoYu WeiLong WR M',
+        img: 'https://i.ibb.co/r5Cj9cv/06.jpg',
+        price: 1300,
+    },
+    {
+        id: 'el7',
+        name: 'Gan 12 M Maglev',
+        img: 'https://i.ibb.co/5hpyzyv/07.jpg',
+        price: 3000,
+    },
+    {
+        id: 'el8',
+        name: 'Valk 3 Elite M',
+        img: 'https://i.ibb.co/7nD9wYH/08.jpg',
+        price: 1500,
+    },
+    {
+        id: 'el9',
+        name: 'MoYu MeiLong Magnetic',
+        img: 'https://i.ibb.co/mbGDbcF/09.jpg',
+        price: 200,
+    },
+    {
+        id: 'el10',
+        name: 'Gan 356 Air S',
+        img: 'https://i.ibb.co/7YKV8RX/10.jpg',
+        price: 1600,
+    },
+    
+];
